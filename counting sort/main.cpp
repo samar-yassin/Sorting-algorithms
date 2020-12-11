876b99cdc8d51244ccdabbaaf4afceed70790e9d
samar-yassin <46316871+samar-yassin@users.noreply.github.com>
2020-12-11 17:53:53 +0200

    Update main.cpp

--- counting sort/main.cpp
+++ counting sort/main.cpp
@@ -2,15 +2,25 @@
 #include <bits/stdc++.h>
 using  namespace std;
 
+int getMax(int arr[],int len){
+    int max=arr[0];
+    for(int i=0; i<len ; i++){
+        if(arr[i]>max) max=arr[i];
+    }
+    return max;
+
+}
+
 void countSort(int arr[]  , int len){
+    int max= getMax(arr,len);
     int output[len];
-    int count[256];
+    int count[max+1];
 
     memset(count,0,sizeof(count));
 
     for(int i=0; i<len ; i++) ++count[arr[i]];
 
-    for(int i =1;i<256;i++) count[i]+=count[i-1];
+    for(int i =1;i<max+1;i++) count[i]+=count[i-1];
 
     for(int i =0 ; i<len ; i++){
         output[count[arr[i]]-1]=arr[i];
